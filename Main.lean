537da9757d30f75936b7fa509f8b493025fb7c3b
SravaniPanu <61602222+SravaniPanu@users.noreply.github.com>
2025-05-14 01:00:56 -0700

    created basic repo for project with mathlib dependency

---
+++ Main.lean
@@ -0,0 +1,4 @@
+import Sp480b6color
+
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
---
+++ Sp480b6color.lean
@@ -0,0 +1,3 @@
+-- This module serves as the root of the `Sp480b6color` library.
+-- Import modules here that should be built as part of the library.
+import Sp480b6color.Basic

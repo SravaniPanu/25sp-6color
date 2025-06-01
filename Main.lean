7091097fd4c0ce5b84532ea57689313ed6f7918c
arbitraryobject <anarbitraryobject@gmail.com>
2025-06-01 16:58:23 -0700

    Outline for the final 6 color proof

--- Main.lean
+++ Main.lean
@@ -1,9 +1,15 @@
-import Sp480b6color
+import Mathlib
 
-#eval isEvenPrime 2
-#eval isEvenPrime 3
-#eval isEvenPrime 4
+import Sp480b6color.Planar_defs
+variable {V : Type u} {G : SimpleGraph V}
+  [Fintype V] [Nonempty V] [DecidableRel G.Adj]
+lemma zero_colorable (h: Planar G) (h1: Fintype.card V = 0) : G.Colorable 6 := by
+    sorry
 
-def main : IO Unit :=
-
-  IO.println s!"Hello!"
+theorem Color (h: Planar G) : (G.Colorable) (6) := by
+    induction' hV: Fintype.card V with n ih
+    case zero =>
+        exact zero_colorable h hV
+    case succ =>
+        obtain ⟨v0, hv0⟩ := Planar.five_or_fewer_vertex h
+        sorry
